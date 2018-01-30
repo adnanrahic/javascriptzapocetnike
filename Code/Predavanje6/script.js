b94af1e7d9fec7e6de070c2832c57db6965abb47
adnanrahic <ado.raha198@gmail.com>
2018-01-30 20:02:07 +0100

    refactor predavanje 6 i 7

--- Code/Predavanje6/script.js
+++ Code/Predavanje6/script.js
@@ -1,1 +1,24 @@
-console.log('Meho je awesome!');
+var x = document.body.children;
+
+// var y = document.getElementById('naslov');
+var y = document.querySelector('h1');
+
+var p = document.createElement('p');
+p.innerText = 'Meho!';
+p.style.color = 'yellow';
+
+y.appendChild(p);
+
+console.dir(y);
+
+
+var obj = {
+  name : 'meho',
+  height : '738237'
+}
+
+buildTable(obj);
+
+function buildTable(obj) {
+  
+}
